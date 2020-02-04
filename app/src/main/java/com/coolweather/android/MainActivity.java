0ea31a055d8ee6c808ad651472b9b87c19db6b05
SaiCateDoan <saicatedoan@qq.com>
2020-02-04 19:05:46 +0800

    完成了SDK的初始化操作。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -7,12 +7,16 @@ import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HeConfig.init("HE2002041551101039","c9741eee6d6c47b38307d802ebb82ec9");
+        HeConfig.switchToFreeServerNode();
         SharedPreferences perfs = PreferenceManager.getDefaultSharedPreferences(this);
         if(perfs.getString("weather",null)!=null){
             Intent intent = new Intent(this,WeatherActivity.class);
--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -106,6 +106,10 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
     }
+    public void newrequestWeather(final String weatherId){
+        
+
+    }
     public void requestWeather(final String weatherId){
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId +
                 "&key=bc0418b57b2d4918819d3974ac1825d9";
